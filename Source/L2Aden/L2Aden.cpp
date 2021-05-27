1782fd84e84dd26463ab23d2b99d5dacb45c01bd
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2021-05-27 16:32:40 +0800

    delete all

--- Source/L2Aden/L2Aden.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "L2Aden.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L2Aden, "L2Aden" );
- 
