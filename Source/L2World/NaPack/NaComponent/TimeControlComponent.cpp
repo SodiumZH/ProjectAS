ebca7c9aae23de40278b06bae12306289379f4da
SodiumZH <hlzhang_1998@outlook.com>
2021-09-11 01:18:18 +0800

    Update time control component

--- Source/L2World/NaPack/NaComponent/TimeControlComponent.cpp
+++ Source/L2World/NaPack/NaComponent/TimeControlComponent.cpp
@@ -1,19 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "NaPack/NaComponent/TimeControlComponent.h"
+#include "TimeControlComponent.h"
 
 
-FTimepointEvent::FTimepointEvent(double Seconds, const FTimepointEventSignature & Event) {
+FTimePointEvent::FTimePointEvent() {
+	TimePointSeconds = 0;
+	EventDelegate = FTimePointEventSignature();
+}
+
+FTimePointEvent::FTimePointEvent(double Seconds, const FTimePointEventSignature & Event) {
 	TimePointSeconds = Seconds;
 	EventDelegate = Event;
+}
+
+FTimeLoopEvent::FTimeLoopEvent() {
+	StartTime = 0;
+	LoopTime = 0;
+	EventDelegate = FTimeLoopEventSignature();
+	NextTime = 0;
 }
 
 FTimeLoopEvent::FTimeLoopEvent(double Start, double Loop, const FTimeLoopEventSignature & Event) {
 	StartTime = Start;
 	LoopTime = Loop;
-	LoopEvent = Event;
-	NextTime = StartTime;
+	EventDelegate = Event;
+	NextTime = Start;
 }
 
 // Sets default values for this component's properties
@@ -32,6 +44,7 @@ void UTimeControlComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TimeInit();
 	
 }
 
@@ -41,6 +54,98 @@ void UTimeControlComponent::TickComponent(float DeltaTime, ELevelTick TickType, 
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	Tick_UpdateTime();
+
+	
+}
+
+
+/* Time */
+
+void UTimeControlComponent::TimeInit() {
+	TimeThisTick = TimeLastTick = TimeBeginPlay = FPlatformTime::Seconds();
+}
+
+void UTimeControlComponent::Tick_UpdateTime() {
+	TimeLastTick = TimeThisTick;
+	TimeThisTick = FPlatformTime::Seconds();
+}
+
+double UTimeControlComponent::GetTime() {
+	return TimeThisTick - TimeBeginPlay;
+}
+
+double UTimeControlComponent::GetTimeLastTick() {
+	return TimeLastTick - TimeBeginPlay;
+}
+
+float UTimeControlComponent::GetTimeSecondsFromBeginPlay() {
+	return (float)GetTime();
+}
+
+void UTimeControlComponent::AddTimePointEvent(FName Name, float TimeSecondsFromNow, const FTimePointEventSignature & Event, bool bForceAdd) {
+
+	if (bForceAdd) {
+		PointEvents.Emplace(Name, FTimePointEvent((double)TimeSecondsFromNow + TimeThisTick, Event));
+	}
+	else if (!PointEvents.Contains(Name)) {
+		PointEvents.Emplace(Name, FTimePointEvent((double)TimeSecondsFromNow + TimeThisTick, Event));
+	}
+
+}
+
+void UTimeControlComponent::AddLoopEvent(FName Name, float TimeSecondsFromNow, float Period, const FTimeLoopEventSignature & Event, bool bForceAdd) {
+	if (bForceAdd) {
+		LoopEvents.Emplace(Name, FTimeLoopEvent((double)TimeSecondsFromNow + TimeThisTick, (double)Period, Event));
+	}
+	else if (!PointEvents.Contains(Name)) {
+		LoopEvents.Emplace(Name, FTimeLoopEvent((double)TimeSecondsFromNow + TimeThisTick, (double)Period, Event));
+	}
+}
+
+void UTimeControlComponent::Tick_PointEvents() {
+
+	for (auto & Elem : PointEvents) {
+		if ((GetTime() >= Elem.Value.TimePointSeconds) && (GetTimeLastTick() < Elem.Value.TimePointSeconds)) {
+			Elem.Value.EventDelegate.ExecuteIfBound();
+			PointEvents.Remove(Elem.Key);
+		}
+	}
+}
+
+void UTimeControlComponent::Tick_LoopEvents() {
+
+	for (auto & Elem : LoopEvents) {
+		if ((GetTime() >= Elem.Value.NextTime) && (GetTimeLastTick() < Elem.Value.NextTime)) {
+			if (Elem.Value.EventDelegate.IsBound()) {
+				Elem.Value.EventDelegate.Execute((float)GetTime());
+			}
+			Elem.Value.NextTime += Elem.Value.LoopTime;
+		}
+	}
+
+}
+
+void UTimeControlComponent::RemoveTimePointEvent(FName Name, bool ExecuteBeforeRemove) {
+
+	FTimePointEvent* EventToRemove = PointEvents.Find(Name);
+	if (EventToRemove) {
+		if(ExecuteBeforeRemove)
+			EventToRemove->EventDelegate.ExecuteIfBound();
+		PointEvents.Remove(Name);
+	}
+
+}
+
+void UTimeControlComponent::RemoveLoopEvent(FName Name, bool ExecuteBeforeRemove) {
+
+	FTimeLoopEvent* EventToRemove = LoopEvents.Find(Name);
+	if (EventToRemove) {
+		if (ExecuteBeforeRemove && EventToRemove->EventDelegate.IsBound())
+			EventToRemove->EventDelegate.Execute((float)GetTime());
+		LoopEvents.Remove(Name);
+		EventToRemove = nullptr;
+	}
+
 }
 
--- Source/L2World/NaPack/NaComponent/TimeControlComponent.h
+++ Source/L2World/NaPack/NaComponent/TimeControlComponent.h
@@ -7,8 +7,8 @@
 #include "TimeControlComponent.generated.h"
 
 
-DECLARE_DELEGATE(FTimePointEventSignature);
-DECLARE_DELEGATE_OneParam(FTimeLoopEventSignature, float);
+DECLARE_DYNAMIC_DELEGATE(FTimePointEventSignature);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FTimeLoopEventSignature, float, TimeNow);
 
 USTRUCT()
 struct FTimePointEvent {
@@ -17,9 +17,11 @@ struct FTimePointEvent {
 
 public:
 
-	double TimePointSeconds;
+	double TimePointSeconds;	// Time from begin play
 
 	FTimePointEventSignature EventDelegate;
+
+	FTimePointEvent();
 
 	FTimePointEvent(double Seconds, const FTimePointEventSignature & Event);
 
@@ -32,13 +34,15 @@ struct FTimeLoopEvent {
 
 public:
 
-	double StartTime;
+	double StartTime;	// Time from begin play
 
 	double LoopTime;
 
-	FTimeLoopEventSignature LoopEvent;
+	FTimeLoopEventSignature EventDelegate;
 
-	double NextTime;
+	double NextTime;	// Time from begin play
+
+	FTimeLoopEvent();
 
 	FTimeLoopEvent(double Start, double Loop, const FTimeLoopEventSignature & Event);
 
@@ -59,34 +63,60 @@ protected:
 
 protected:
 
-	double StartTime = 0.0;
+	/* Time recording */
 
+	double TimeBeginPlay = 0.0;
+
+	// Absolute time
 	double TimeLastTick = 0.0;
 
-	TMap<FName, FTimePointEvent> Events;
+	// Absolute time
+	double TimeThisTick = 0.0;
+
+	// Get time from begin play.
+	double GetTime();
+
+	// Get time of last tick from begin play
+	double GetTimeLastTick();
+
+	void TimeInit();
+
+	void Tick_UpdateTime();
+
+	/* Events */
+
+	TMap<FName, FTimePointEvent> PointEvents;
 
 	TMap<FName, FTimeLoopEvent> LoopEvents;
+
+	void Tick_PointEvents();
+	
+	void Tick_LoopEvents();
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-
 	/* Get time seconds from begin play of this component */
 	UFUNCTION(BlueprintPure, meta = (DefaultToSelf), Category="NaPack|Component")
-	float GetTimeSecondsFromStart();
+	float GetTimeSecondsFromBeginPlay();
 
 	/** Add time point event using time seconds from now. The event will be executed only once.
 	* @Param ForceAdd If true, when the name is existing, the new event will override the old one. Or new event will not be added if the name is existing.
 	*/
-	UFUNCTION(BlueprintPure, Category = "NaPack|Component")
-	void AddTimePointEvent(FName Name, meta = (DefaultToSelf), float TimeSecondsFromNow, const FTimePointEventSignature & Event, bool bForceAdd = true);
+	UFUNCTION(BlueprintCallable, meta = (DefaultToSelf), Category = "NaPack|Component")
+	void AddTimePointEvent(FName Name, float TimeSecondsFromNow, const FTimePointEventSignature & Event, bool bForceAdd = true);
 
 	/* Add loop event using time seconds from now as the first execution time.
 	* @Param ForceAdd If true, when the name is existing, the new event will override the old one. Or new event will not be added if the name is existing.
 	*/
-	UFUNCTION(BlueprintPure, meta = (DefaultToSelf), Category = "NaPack|Component")
-	void AddLoopEvent(FName Name, float TimeSecondsFromNow, const FTimeLoopEventSignature & Event, bool bForceAdd = true);
+	UFUNCTION(BlueprintCallable, meta = (DefaultToSelf), Category = "NaPack|Component")
+	void AddLoopEvent(FName Name, float TimeSecondsFromNow, float Period, const FTimeLoopEventSignature & Event, bool bForceAdd = true);
 
-	
+	UFUNCTION(BlueprintCallable, Category = "NaPack|Component")
+	void RemoveTimePointEvent(FName Name, bool ExecuteBeforeRemove = false);
+
+	UFUNCTION(BlueprintCallable, Category = "NaPack|Component")
+	void RemoveLoopEvent(FName Name, bool ExecuteBeforeRemove = false);
+
 };
