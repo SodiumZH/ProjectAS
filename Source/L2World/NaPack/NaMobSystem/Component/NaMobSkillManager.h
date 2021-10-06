f8a1d2aa69f84d06ab13fcaef119077604c51ed4
SodiumZH <hlzhang_1998@outlook.com>
2021-10-06 21:32:59 +0800

    split mob functions
    将mob中关于skill、weapon等功能组件化，拆分功能

---
+++ Source/L2World/NaPack/NaMobSystem/Component/NaMobSkillManager.h
@@ -0,0 +1,23 @@
+/* By Sodium */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "../../NaGlobalHeader.h"
+#include "NaMobSkillManager.generated.h"
+
+/* NaMob Skill Manager is an interface component attached only to NaMob for handling skills (based on class ANaMobSkill). */
+
+
+UCLASS(BlueprintType)
+class NAPACK_API UNaMobSkillManager : public UActorComponent {
+	
+	GENERATED_BODY()
+
+public:
+
+
+
+
+};
