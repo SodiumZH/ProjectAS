c5f2194f68770e673ad48b23ab7e50a530754b9f
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-02-24 01:28:40 +0800

    remove NaItemEntry

--- Plugins/NaItemSystem/Source/NaItemSystem/Public/NaItemEntry.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "CoreMinimal.h"
-#include "NaItemEntry.generated.h"
-
-// Legacy item entry structs (FNaItemDescriptor, FNaItemEntry, FNaActiveItemEntry) have been removed.
-// Use UNaItemStack instead. See NaItemStack.h.
